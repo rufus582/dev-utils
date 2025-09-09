cc1cd743f52f93a10d3f703d3bfeeb515046d028
rufus582 <rufus5802@gmail.com>
2025-09-09 22:11:29 +0530

    Added dummy page for root path

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Link } from "react-router";
 import { RouterProvider as ReactRouterProvider } from "react-router/dom";
 import HomeLayout from "@/components/layout/home-layout/home-layout";
 import { routeDefinitions } from "./route-definitions";
@@ -6,7 +6,13 @@ import { routeDefinitions } from "./route-definitions";
 const router = createBrowserRouter([
   {
     Component: HomeLayout,
-    children: routeDefinitions
+    children: [
+      {
+        path: '/',
+        element: <Link to='/jq'>Load</Link>
+      },
+      ...routeDefinitions
+    ]
   },
 ]);
 
